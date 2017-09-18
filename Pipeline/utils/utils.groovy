1b8dc581356b787330dab652a565a00e91278af5
Jonas Vinther <jrvinther@gmail.com>
2017-09-18 11:10:16 +0200

    Test external file

--- Pipeline/utils/utils.groovy
+++ Pipeline/utils/utils.groovy
@@ -1,4 +1,10 @@
 def log(msg) {
     println("========== " + msg)
 }
+
+def getLatestArtifactVersion(from) {
+    def artifactoryBase64AuthInfo = generateArtifactoryAuthInfo()
+    return powershell(script: ". '.\\Pipeline\\build_scripts\\GetLatestArtifact.ps1' ${from} ${artifactoryBase64AuthInfo} ${artifactoryApiPath} ${artifactoryRepository}", returnStdout: true).trim()
+}
+
 return this
