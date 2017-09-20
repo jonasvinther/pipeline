501d5884b4ee41e3ce987f8ab5f3ce67e14ee81e
Jonas Vinther <jrvinther@gmail.com>
2017-09-20 09:43:31 +0200

    Test

--- Pipeline/build_scripts/artifactory.ps1
+++ Pipeline/build_scripts/artifactory.ps1
@@ -1,6 +1,7 @@
 
 $ScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
-. $ScriptDirectory + "\config.ps1"
 
-return $ScriptDirectory
+
+
+return "$ScriptDirectory\config.ps1"
